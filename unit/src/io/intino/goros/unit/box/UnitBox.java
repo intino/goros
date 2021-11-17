a127be5e5d6803d21c0ad4f44f81de0ec514eb7f
Mario Caballero Ramírez <mariocaballero@siani.es>
2021-11-17 10:18:07 +0000

    documents, unit: added max-idle-connections parameter

--- unit/src/io/intino/goros/unit/box/UnitBox.java
+++ unit/src/io/intino/goros/unit/box/UnitBox.java
@@ -104,6 +104,7 @@ public class UnitBox extends AbstractBox {
 		if (key.equals("jdbc-datasource")) return "Jdbc.DataSource";
 		if (key.equals("jdbc-type")) return "Jdbc.Type";
 		if (key.equals("jdbc-max-active-connections")) return "Jdbc.MaxActiveConnections";
+		if (key.equals("jdbc-max-idle-connections")) return "Jdbc.MaxIdleConnections";
 		if (key.equals("jdbc-remove-abandoned-timeout")) return "Jdbc.RemoveAbandonedTimeout";
 		if (key.equals("jdbc-url")) return "Jdbc.Url";
 		if (key.equals("jdbc-user")) return "Jdbc.User";
